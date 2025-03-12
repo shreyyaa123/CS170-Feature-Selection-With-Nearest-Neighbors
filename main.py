c339f92aeb686b15c703f4084cc39d391c73d203
shreyyaa123 <mohanshreya1@gmail.com>
2025-03-12 14:16:00 -0700

    fixed significant digits for accuracy and time

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def forward_selection(data):
 
     print("Accuracy using only the empty set:")
     best_accuracy_so_far = leave_one_out_cross_validation(data[:, [0]]) 
-    print(f"Using feature(s) {current_set_of_features} accuracy is: {best_accuracy_so_far}")
+    print(f"Using feature(s) {current_set_of_features} accuracy is: {best_accuracy_so_far:.3f}")
 
     print()
     print("Forward Selection starting now..")
@@ -57,7 +57,7 @@ def forward_selection(data):
 
                 selected_data = data[:, temp_feature_set]  # Select data corresponding to the selected features
                 accuracy = leave_one_out_cross_validation(selected_data)  # Evaluate model with these features
-                print(f"Using feature(s) {current_set_of_features + [feature]} accuracy is: {accuracy}")
+                print(f"Using feature(s) {current_set_of_features + [feature]} accuracy is: {accuracy:.3f}")
                 print()
                 if accuracy > best_accuracy_for_current_level:  # Updating best_accuracy at the current level
                     best_accuracy_for_current_level = accuracy
@@ -67,17 +67,17 @@ def forward_selection(data):
         if feature_to_add is not None:
             current_set_of_features.append(feature_to_add)  # Add the best feature to the selected set
             print()
-            print(f"Feature set {current_set_of_features} was best, {best_accuracy_for_current_level}")
+            print(f"Feature set {current_set_of_features} was best, {best_accuracy_for_current_level:.3f}")
             print()
 
         if best_accuracy_for_current_level > best_accuracy_so_far:
             best_accuracy_so_far = best_accuracy_for_current_level
             best_feature_set = current_set_of_features.copy()
 
-    print(f'Finished search: Best feature set is {best_feature_set} with an accuracy of {best_accuracy_so_far}')
+    print(f'Finished search: Best feature set is {best_feature_set} with an accuracy of {best_accuracy_so_far:.3f}')
 
     end_time = time.time()  
-    print(f"Time taken for Forward Selection: {end_time - start_time:.4f} seconds\n")
+    print(f"Time taken for Forward Selection: {end_time - start_time:.2f} seconds\n")
     return current_set_of_features
 
 
@@ -90,7 +90,7 @@ def backward_elimination(data):
     
     print("Accuracy with the full feature set")
     best_accuracy_so_far = leave_one_out_cross_validation(data[:, [0] + current_set_of_features]) #calculates accuracy of full feature set
-    print(f"Using feature(s) {current_set_of_features} accuracy is: {best_accuracy_so_far}")
+    print(f"Using feature(s) {current_set_of_features} accuracy is: {best_accuracy_so_far:.3f}")
     print()
     print("Backward elimination starting now...")
 
@@ -103,7 +103,7 @@ def backward_elimination(data):
             
             selected_data = data[:, [0] + temp_feature_set]  # Select data corresponding to the selected features
             accuracy = leave_one_out_cross_validation(selected_data)  # Evaluate model with these features
-            print(f"Using feature(s) {temp_feature_set} accuracy is: {accuracy}")
+            print(f"Using feature(s) {temp_feature_set} accuracy is: {accuracy:.3f}")
             print()
             
             if accuracy > best_accuracy_for_current_level:  # Updating best_accuracy at the current level
@@ -113,17 +113,17 @@ def backward_elimination(data):
         if feature_to_remove is not None:
             current_set_of_features.remove(feature_to_remove)  # Remove the feature
             print()
-            print(f"Feature set {current_set_of_features} was best, {best_accuracy_for_current_level}")
+            print(f"Feature set {current_set_of_features} was best, {best_accuracy_for_current_level:.3f}")
             print()
 
         if best_accuracy_for_current_level > best_accuracy_so_far: #Updates the best feature set to get the feature set with the highest accuracy
             best_accuracy_so_far = best_accuracy_for_current_level
             best_feature_set = current_set_of_features.copy()
     
-    print(f'Finished search: Best feature set is {best_feature_set} with an accuracy of {best_accuracy_so_far}')
+    print(f'Finished search: Best feature set is {best_feature_set} with an accuracy of {best_accuracy_so_far:.3f}')
 
     end_time = time.time() 
-    print(f"Time taken for Backward Elimination: {end_time - start_time:.4f} seconds\n")
+    print(f"Time taken for Backward Elimination: {end_time - start_time:.2f} seconds\n")
     return best_feature_set
     
 def get_user_choice():
